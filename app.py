6ba42ed7bdef14b60af641f0d29078b797c73ab8
Parth Bhardwaj <138441221+ParthBhardwaj7@users.noreply.github.com>
2025-07-20 19:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,3 +63,6 @@ if st.button('Predict Price'):
 
     query = query.reshape(1,12)
     st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
+st.markdown("---")
+st.markdown("**Made with ❤️ by Parth**")
+
